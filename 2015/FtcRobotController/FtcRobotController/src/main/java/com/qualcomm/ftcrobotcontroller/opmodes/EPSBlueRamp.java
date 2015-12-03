7c5293085ef6aacedd99a83a64166dd8cf599f44
FisherH <Fisher@eschreiber.eastsideprep.local>
2015-12-03 15:48:23 -0800

    Added Autonomous Routines
    Added two autonomous routines per possible alliance.

---
+++ 2015/FtcRobotController/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EPSBlueRamp.java
@@ -0,0 +1,56 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by supertrevor on 2015-11-13.
+ */
+
+public class EPSBlueRamp extends LinearOpMode {
+
+    final static double MOTOR_POWER = 0.50; // Higher values will cause the robot to move faster
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // set up the hardware devices we are going to use
+        motorLeft = hardwareMap.dcMotor.get("motor_2");
+        motorRight = hardwareMap.dcMotor.get("motor_1");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // wait for the start button to be pressed
+        waitForStart();
+
+        motorRight.setPower(MOTOR_POWER);
+        motorLeft.setPower(MOTOR_POWER);
+
+        sleep(2000);
+
+        motorRight.setPower(-MOTOR_POWER);
+
+        sleep(1000);
+
+        motorRight.setPower(MOTOR_POWER);
+
+        sleep(2200);
+
+        motorRight.setPower(-MOTOR_POWER);
+
+        sleep(300);
+
+        motorRight.setPower(MOTOR_POWER);
+
+        sleep(2000);
+
+        // stop the motors
+        motorRight.setPower(0);
+        motorLeft.setPower(0);
+    }
+}
+
