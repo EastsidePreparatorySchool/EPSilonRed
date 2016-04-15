b3d3d3f46db81cd202f3d6394cb77a79ffd062f7
Trevor Nielsen <trevor.nielsen@gmail.com>
2016-04-15 07:49:46 -0700

    Updated driving controls

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EPSTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EPSTeleOp.java
@@ -11,29 +11,33 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 public class EPSTeleOp extends OpMode {
-    DcMotor motorRight;
-    DcMotor motorLeft;
+    DcMotor motorRight1;
+    DcMotor motorLeft1;
+    DcMotor motorRight2;
+    DcMotor motorLeft2;
 
-    DcMotor motorArmAngle;
+    /*DcMotor motorArmAngle;
     DcMotor motorActuator;
 
     DcMotor motorChurroGrabber;
     DcMotor motorWinch;
 
-    Servo servoPlow;
+    Servo servoPlow;*/
 
     int precisionModeDrive;
     int precisionModeArm;
 
     @Override
     public void init() {
-        motorRight = hardwareMap.dcMotor.get("motor_2");
-        motorLeft = hardwareMap.dcMotor.get("motor_1");
-        motorArmAngle = hardwareMap.dcMotor.get("motor_3");
+        motorRight1 = hardwareMap.dcMotor.get("motor_2b");
+        motorRight2 = hardwareMap.dcMotor.get("motor_2a");
+        motorLeft1 = hardwareMap.dcMotor.get("motor_1a");
+        motorLeft2 = hardwareMap.dcMotor.get("motor_1b");
+        /*motorArmAngle = hardwareMap.dcMotor.get("motor_3");
         motorActuator = hardwareMap.dcMotor.get("motor_4");
         motorChurroGrabber = hardwareMap.dcMotor.get("motor_5");
         motorWinch = hardwareMap.dcMotor.get("motor_6");
-        servoPlow = hardwareMap.servo.get("servo_1");
+        servoPlow = hardwareMap.servo.get("servo_1");*/
 
         precisionModeDrive = 0;
         precisionModeArm = 0;
@@ -41,17 +45,19 @@ public class EPSTeleOp extends OpMode {
 
     @Override
     public void start() {
-        motorLeft.setDirection(DcMotor.Direction.FORWARD);
-        motorRight.setDirection(DcMotor.Direction.FORWARD);
+        motorLeft1.setDirection(DcMotor.Direction.FORWARD);
+        motorRight1.setDirection(DcMotor.Direction.FORWARD);
+        motorLeft2.setDirection(DcMotor.Direction.REVERSE);
+        motorRight2.setDirection(DcMotor.Direction.REVERSE);
 
-        motorArmAngle.setDirection(DcMotor.Direction.REVERSE);
+       /* motorArmAngle.setDirection(DcMotor.Direction.REVERSE);
         motorActuator.setDirection(DcMotor.Direction.REVERSE);
 
         motorChurroGrabber.setDirection((DcMotor.Direction.FORWARD));
         motorWinch.setDirection((DcMotor.Direction.FORWARD));
 
         servoPlow.setDirection(Servo.Direction.FORWARD);
-
+*/
     }
 
     @Override
@@ -61,36 +67,40 @@ public class EPSTeleOp extends OpMode {
         // 1 is full down
         // direction: left_stick_x ranges from -1 to 1, where -1 is full left
         // and 1 is full right
-        float leftTread = -gamepad1.right_stick_y;
-        float rightTread = gamepad1.left_stick_y;
+        float latitude = -gamepad1.right_stick_y;
+        float longitude = gamepad1.right_stick_x;
         float armAngle = gamepad2.right_stick_y;
         float actuator = gamepad2.left_stick_y;
 
         // clip the right/left values so that the values never exceed +/- 1
-        rightTread = Range.clip(rightTread, -1, 1);
-        leftTread = Range.clip(leftTread, -1, 1);
+        longitude = Range.clip(longitude, -1, 1);
+        latitude = Range.clip(latitude, -1, 1);
         armAngle = Range.clip(armAngle, -1, 1);
         actuator = Range.clip(actuator, -1, 1);
 
         // scale the joystick value to make it easier to control
         // the robot more precisely at slower speeds.
-        rightTread = (float)scaleInput(rightTread);
-        leftTread =  (float)scaleInput(leftTread);
+        longitude = (float)scaleInput(longitude);
+        latitude =  (float)scaleInput(latitude);
         armAngle = (float)scaleInput(armAngle);
         actuator =  (float)scaleInput(actuator);
 
         // write the values to the motors
         if(precisionModeDrive == 1) {
-            motorRight.setPower(rightTread / 2f);
-            motorLeft.setPower(leftTread / 2f);
+            motorRight1.setPower(longitude / 2f);
+            motorLeft1.setPower(latitude / 2f);
+            motorRight2.setPower(longitude / 2f);
+            motorLeft2.setPower(latitude / 2f);
         }
 
         else {
-            motorRight.setPower(rightTread);
-            motorLeft.setPower(leftTread);
+            motorRight1.setPower(longitude);
+            motorLeft1.setPower(latitude);
+            motorRight2.setPower(longitude);
+            motorLeft2.setPower(latitude);
         }
 
-        if(precisionModeArm == 1) {
+        /*if(precisionModeArm == 1) {
             motorArmAngle.setPower(armAngle * 0.2f);
             motorActuator.setPower(actuator / 4f);
         }
@@ -98,10 +108,10 @@ public class EPSTeleOp extends OpMode {
         else {
             motorArmAngle.setPower(armAngle * 0.30f);
             motorActuator.setPower(actuator / 2f);
-        }
+        }*/
 
 
-        if(gamepad1.x == true)
+       /* if(gamepad1.x == true)
         {
             motorChurroGrabber.setPower(1f);
         }
@@ -123,7 +133,7 @@ public class EPSTeleOp extends OpMode {
         else if(gamepad1.dpad_down == true) {
             servoPlow.setPosition(1.0);
         }
-
+        */
         if(gamepad1.a == true) {
             precisionModeDrive = 1;
         }
@@ -141,7 +151,7 @@ public class EPSTeleOp extends OpMode {
             precisionModeArm = 0;
         }
 
-        if(gamepad2.x == true) {
+       /* if(gamepad2.x == true) {
             motorWinch.setPower(0.9f);
         }
 
@@ -151,7 +161,7 @@ public class EPSTeleOp extends OpMode {
 
         else {
             motorWinch.setPower(0f);
-        }
+        }*/
 
 		/*
 		 * Send telemetry data back to driver station. Note that if we are using
@@ -160,8 +170,8 @@ public class EPSTeleOp extends OpMode {
 		 * are currently write only.
 		 */
         telemetry.addData("Text", "*** Robot Data***");
-        telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", leftTread));
-        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", rightTread));
+        telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", latitude));
+        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", longitude));
     }
 
     /*
